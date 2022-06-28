dd5584bebc3cfc3b523e13cd24c347277a0c0130
Mauri <maurisilva1972@gmail.com>
2022-06-28 19:28:50 -0300

    funcion para solo numeros y letras

--- Vista/Register.cs
+++ Vista/Register.cs
@@ -16,5 +16,54 @@ namespace Vista
         {
             InitializeComponent();
         }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyNumber(sender, e);
+        }
+
+        private void txtDNI_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyNumber(sender, e);
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyLetter(sender,e);
+        }
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyLetter(sender, e);
+
+
+        }
+
+        /* FUNCION PARA SOLO LETRAS Y NUMEROS */
+
+        public void onlyNumber(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+            return;
+
+        }
+
+        public void onlyLetter(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+
+        }
+
     }
 }
