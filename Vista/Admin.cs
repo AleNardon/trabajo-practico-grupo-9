eb8f6603d86836598f88a6a7dcf272a7bae82cad
Mauri <maurijunk02@gmail.com>
2022-07-06 20:30:39 -0300

    agregue dgv y su display

---
+++ Vista/Admin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Vista
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+
+        public string conexion = "Data Source= SISTEMAPEAJE.s3db;Version=3;New=False;Compress=True;";
+
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            SQLiteConnection cn = new SQLiteConnection(conexion);
+
+            string query = "select * from USUARIOS";
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, cn);
+
+            da.SelectCommand.CommandType = CommandType.Text;
+
+            cn.Open();
+            da.Fill(dt);
+
+            dgvUsuarios.DataSource = dt;
+
+        }
+    }
+}
