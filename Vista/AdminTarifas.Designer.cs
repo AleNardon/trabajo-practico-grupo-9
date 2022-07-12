c420d65fe1a3d1459dfb6f2e77fc3432acec3e2b
Juan <juancruzrodriguez3@gmail.com>
2022-07-12 12:57:55 -0300

    dis tarifas

--- Vista/AdminTarifas.Designer.cs
+++ Vista/AdminTarifas.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCamión = new System.Windows.Forms.TextBox();
+            this.txtMoto = new System.Windows.Forms.TextBox();
+            this.txtAuto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camión";
+            // 
+            // txtCamión
+            // 
+            this.txtCamión.Location = new System.Drawing.Point(43, 110);
+            this.txtCamión.Name = "txtCamión";
+            this.txtCamión.Size = new System.Drawing.Size(118, 26);
+            this.txtCamión.TabIndex = 2;
+            // 
+            // txtMoto
+            // 
+            this.txtMoto.Location = new System.Drawing.Point(288, 110);
+            this.txtMoto.Name = "txtMoto";
+            this.txtMoto.Size = new System.Drawing.Size(118, 26);
+            this.txtMoto.TabIndex = 3;
+            // 
+            // txtAuto
+            // 
+            this.txtAuto.Location = new System.Drawing.Point(167, 110);
+            this.txtAuto.Name = "txtAuto";
+            this.txtAuto.Size = new System.Drawing.Size(118, 26);
+            this.txtAuto.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Auto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Moto";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.Navy;
+            this.btnActualizar.FlatAppearance.BorderSize = 0;
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnActualizar.Location = new System.Drawing.Point(43, 174);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(156, 49);
+            this.btnActualizar.TabIndex = 7;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(321, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Indique el tipo de vehículo";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Navy;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(245, 174);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(161, 49);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click_1);
+            // 
+            // AdminTarifas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminTarifas";
+            this.ClientSize = new System.Drawing.Size(499, 303);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAuto);
+            this.Controls.Add(this.txtMoto);
+            this.Controls.Add(this.txtCamión);
+            this.Controls.Add(this.label1);
+            this.Name = "AdminTarifas";
+            this.Text = "S";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCamión;
+        private System.Windows.Forms.TextBox txtMoto;
+        private System.Windows.Forms.TextBox txtAuto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
